3cef2cf95be19c9ad691fa0afa14068f8b54d6e1
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 11:39:25 +0200

    Add another (failing) test for capitalize() function

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -13,10 +13,12 @@ test("capitalize string when first letter is lowercase", () => {
   });
 });
 
-test("capitalize string when first letter is uppercase", () => {
+test("capitalize string when first character is not a lowercase letter", () => {
   const dataArray = [
     ["Hello world!", "Hello world!"],
     ["I'm a string!", "I'm a string!"],
+    ["!important", "!important"],
+    ["44cats", "44cats"],
   ];
 
   dataArray.forEach((data) => {
@@ -27,10 +29,17 @@ test("capitalize string when first letter is uppercase", () => {
 });
 
 test("capitalize a one-character string", () => {
-  const dataArray = [
-    ["h", "H"],
-    ["A", "A"],
-  ];
+  const dataArray = [["h", "H"]];
+
+  dataArray.forEach((data) => {
+    const actual = capitalize(data[0]);
+    const expected = data[1];
+    expect(actual).toBe(expected);
+  });
+});
+
+test("capitalize a zero-character string", () => {
+  const dataArray = [["", ""]];
 
   dataArray.forEach((data) => {
     const actual = capitalize(data[0]);
