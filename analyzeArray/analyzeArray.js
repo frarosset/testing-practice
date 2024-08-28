b027d7aa7fb1d34347fef912aa9176fe94b0974c
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-29 00:40:53 +0200

    Make the previous tests work for calculator object

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,8 +1,12 @@
 export default function analyzeArray(arr) {
-  return {
-    average: 4,
-    min: 1,
-    max: 8,
-    length: 6,
-  };
+  const obj = {};
+
+  const sum = arr.reduce((sum, item) => sum + item, 0);
+
+  obj.min = Math.min(...arr);
+  obj.max = Math.max(...arr);
+  obj.length = arr.length;
+  obj.average = sum / obj.length;
+
+  return obj;
 }
