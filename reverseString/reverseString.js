f91ddc74f529d80fc804a3e640e140f4ba85a8f0
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 15:21:26 +0200

    Make the previous test work for reverseString() function

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,8 @@
 export default function reverseString(string) {
+  if (!(typeof string === "string" || string instanceof String)) {
+    throw new Error("Not a string");
+  }
+
   const arr = string.split("");
   const arrReversed = arr.reduce((arrRev, item) => {
     arrRev.unshift(item);
