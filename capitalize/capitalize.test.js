cbadbbb466e549f4d3b2a74b8bd09a24af740143
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 11:27:35 +0200

    Refactor the test for capitalize() function to avoid duplication

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,15 +1,14 @@
 import capitalize from "./capitalize.js";
 
-test("capitalize string when first letter is lowercase (test 1)", () => {
-  const data = "hello world!";
-  const actual = capitalize(data);
-  const expected = "Hello world!";
-  expect(actual).toBe(expected);
-});
+test("capitalize string when first letter is lowercase", () => {
+  const dataArray = [
+    ["hello world!", "Hello world!"],
+    ["i'm a string!", "I'm a string!"],
+  ];
 
-test("capitalize string when first letter is lowercase (test 2)", () => {
-  const data = "i'm a string!";
-  const actual = capitalize(data);
-  const expected = "I'm a string!";
-  expect(actual).toBe(expected);
+  dataArray.forEach((data) => {
+    const actual = capitalize(data[0]);
+    const expected = data[1];
+    expect(actual).toBe(expected);
+  });
 });
