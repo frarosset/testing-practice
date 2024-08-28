ab986bfc44b2b82c21bffd5f1d5fbfc70d1c3417
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-29 00:52:45 +0200

    Make the previous tests work for calculator object

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,4 +1,8 @@
 export default function analyzeArray(arr) {
+  if (!isArrayOfNumbers(arr)) {
+    throw new Error("Not an array of numbers");
+  }
+
   const obj = {};
 
   const sum = arr.reduce((sum, item) => sum + item, 0);
@@ -9,4 +13,22 @@ export default function analyzeArray(arr) {
   obj.average = sum / obj.length;
 
   return obj;
+}
+
+function isNumeric(num) {
+  return typeof num === "number";
+}
+
+function isArray(data) {
+  return Array.isArray(data);
+}
+
+function isArrayOfNumbers(arr) {
+  if (!isArray(arr)) {
+    return false;
+  }
+
+  // Test that each element is a number
+  const arrOfNumbers = arr.filter((item) => isNumeric(item));
+  return arrOfNumbers.length === arr.length;
 }
