ea33485d488be8d3326555a8bbb938513b7c1cd3
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 12:05:42 +0200

    Make the previous test work for capitalize() function

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,4 +1,8 @@
 export default function capitalize(string) {
+  if (!(typeof string === "string" || string instanceof String)) {
+    throw new Error("Not a string");
+  }
+
   if (string.length == 0) {
     return string;
   }
