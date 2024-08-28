c5cec32045493b015de8c8f993b6e6146b238252
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 11:43:17 +0200

    Make the previous test work for capitalize() function

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,4 +1,7 @@
 export default function capitalize(string) {
+  if (string.length == 0) {
+    return string;
+  }
   const firstLetter = string[0];
   const restOfTheString = string.slice(1);
   return [firstLetter.toUpperCase(), restOfTheString].join("");
