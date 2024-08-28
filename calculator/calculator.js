ed7456065896572cf97cc7c1c8695f623ea43165
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 16:45:11 +0200

    Make the previous test work for calculator object

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,7 +1,14 @@
 export class Calculator {
   constructor() {}
 
+  #isNotNumeric(num) {
+    return typeof num !== "number";
+  }
+
   add(num1, num2) {
+    if (this.#isNotNumeric(num1) || this.#isNotNumeric(num2)) {
+      throw new Error("Not numeric inputs");
+    }
     return num1 + num2;
   }
 }
