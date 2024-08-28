ae840b2fae984432bc3bb8152815276d944fcd53
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 11:28:55 +0200

    Add more tests for capitalize() function
    Note that these tests already work. This has been added to be sure
    future modifications do not break this behavior.

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -12,3 +12,29 @@ test("capitalize string when first letter is lowercase", () => {
     expect(actual).toBe(expected);
   });
 });
+
+test("capitalize string when first letter is uppercase", () => {
+  const dataArray = [
+    ["Hello world!", "Hello world!"],
+    ["I'm a string!", "I'm a string!"],
+  ];
+
+  dataArray.forEach((data) => {
+    const actual = capitalize(data[0]);
+    const expected = data[1];
+    expect(actual).toBe(expected);
+  });
+});
+
+test("capitalize a one-character string", () => {
+  const dataArray = [
+    ["h", "H"],
+    ["A", "A"],
+  ];
+
+  dataArray.forEach((data) => {
+    const actual = capitalize(data[0]);
+    const expected = data[1];
+    expect(actual).toBe(expected);
+  });
+});
