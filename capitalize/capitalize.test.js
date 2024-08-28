27d51235e8f39c63c4501886ffd4819bc535f72d
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 11:15:50 +0200

    Add another (failing) test for capitalize() function

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,8 +1,15 @@
 import capitalize from "./capitalize.js";
 
-test("capitalize string when first letter is lowercase", () => {
+test("capitalize string when first letter is lowercase (test 1)", () => {
   const data = "hello world!";
   const actual = capitalize(data);
   const expected = "Hello world!";
+  expect(actual).toBe(expected);
+});
+
+test("capitalize string when first letter is lowercase (test 2)", () => {
+  const data = "i'm a string!";
+  const actual = capitalize(data);
+  const expected = "I'm a string!";
   expect(actual).toBe(expected);
 });
