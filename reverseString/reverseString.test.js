aefbb9996c9ac2fa9186200666d1898dab9cde59
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 12:40:24 +0200

    Add more tests for reverseString() function
    Note that these tests already work. This has been added to be sure
    future modifications do not break this behavior.

--- reverseString/reverseString.test.js
+++ reverseString/reverseString.test.js
@@ -1,21 +1,15 @@
 import reverseString from "./reverseString.js";
 
-<<<<<<< HEAD
-test("reverse string", () => {
-  const data = "hello world!";
-  const actual = reverseString(data);
-  const expected = "!dlrow olleh";
-  expect(actual).toBe(expected);
-=======
 test("reverse string", () => {
   const dataArray = [
     ["hello world!", "!dlrow olleh"],
     ["I'm a string", "gnirts a m'I"],
+    ["hellolleh", "hellolleh"],
+    ["", ""],
   ];
   dataArray.forEach((data) => {
     const actual = reverseString(data[0]);
     const expected = data[1];
     expect(actual).toBe(expected);
   });
->>>>>>> eadde37 (Add another (failing) test for reverseString() function)
 });
