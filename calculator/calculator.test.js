02608c2452c1fd2fe9862e7f58fae89d3f131d71
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-28 16:04:10 +0200

    Add more tests for calculator object
    Note that these tests already work. This has been added to be sure
    future modifications do not break this behavior.

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -12,3 +12,15 @@ test("add integer numbers", () => {
     expect(actual).toBe(expected);
   });
 });
+
+test("add floating point numbers", () => {
+  const dataArray = [
+    [1.9, 2.8, 4.7],
+    [0.1, 0.2, 0.3],
+  ];
+  dataArray.forEach((data) => {
+    const actual = calculator.add(data[0], data[1]);
+    const expected = data[2];
+    expect(actual).toBeCloseTo(expected);
+  });
+});
