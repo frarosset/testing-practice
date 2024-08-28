dec021b1682cae000c0451f308a95502ab8bd8c4
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-29 00:04:39 +0200

    Make the previous tests work for calculator object

--- caesarCipher/caesarCipher.js
+++ caesarCipher/caesarCipher.js
@@ -1,3 +1,11 @@
+const ACode = "A".charCodeAt();
+const ZCode = "Z".charCodeAt();
+const aCode = "a".charCodeAt();
+const zCode = "z".charCodeAt();
+const azCodeDiff = zCode - aCode + 1;
+const lowercaseInterval = [aCode, zCode, azCodeDiff];
+const uppercaseInterval = [ACode, ZCode, azCodeDiff];
+
 export default function caesarCipher(string, shiftFactor) {
   const stringArr = string.split("");
 
@@ -10,18 +18,27 @@ export default function caesarCipher(string, shiftFactor) {
 
 function encodeChar(char, shiftFactor) {
   const charCode = char.charCodeAt();
+
+  const charInterval = isLowerCase(char)
+    ? lowercaseInterval
+    : uppercaseInterval;
+
   const shiftedCharCode = charCode + shiftFactor;
-  const wrappedShiftedCharCode = wrapChar(shiftedCharCode);
+  const wrappedShiftedCharCode = wrapChar(shiftedCharCode, charInterval);
+
   return String.fromCharCode(wrappedShiftedCharCode);
 }
 
-const aCode = "a".charCodeAt();
-const zCode = "z".charCodeAt();
-const azCodeDiff = zCode - aCode + 1;
-
-function wrapChar(charCode) {
-  while (charCode > zCode) {
-    charCode -= azCodeDiff;
+function wrapChar(charCode, [minCode, maxCode, diff]) {
+  while (charCode < minCode) {
+    charCode += diff;
+  }
+  while (charCode > maxCode) {
+    charCode -= diff;
   }
   return charCode;
+}
+
+function isLowerCase(str) {
+  return str === str.toLowerCase() && str !== str.toUpperCase();
 }
